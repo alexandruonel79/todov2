b461a7a252b0d9772fa865b0602367725d046753
alexandruonel79 <alexandruonel79@gmail.com>
2022-12-13 19:01:45 +0200

    mai e

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var listaTaskuri = [];//all tasks
+var contor = 0;
+
 function addTask() {
         let textNou = document.querySelector('#numeTaskNou').value;
 
-        if (textNou != "") {
+        if (textNou != "") { //nu am trimis alerta pentru task gol, doar este ignorat
                 let taskuri = document.getElementsByClassName("adaugate")[0];
 
                 if (verificareExistenta(textNou) == 1) {
@@ -14,14 +17,17 @@ function addTask() {
 
                 let buton = document.createElement("button");
                 buton.innerHTML = "✔";
+                buton.setAttribute("onclick", "isCompleted(this)");
                 buton.classList.add("check");
 
                 let paragrafe = document.createElement("p");
                 paragrafe.innerHTML = textNou;
+                paragrafe.setAttribute("contenteditable", "false");
                 paragrafe.classList.add("paragrafe");
 
                 let butonEdit = document.createElement("button");
                 butonEdit.innerHTML = "Edit";
+                butonEdit.setAttribute("onclick", "editTask(this.parentElement)");
                 butonEdit.classList.add("edit");
 
                 let butonDelete = document.createElement("button");
@@ -35,6 +41,8 @@ function addTask() {
                 element.appendChild(butonDelete);
 
                 taskuri.appendChild(element);
+                listaTaskuri[contor] = element;
+                contor++;
         }
 }
 function verificareExistenta(textNou) {
@@ -55,9 +63,25 @@ function removeTask(element) {
         let el = element;
         el.remove();
 }
+function editTask(element) {
+        let el = element.getElementsByClassName("paragrafe")[0];
+        if (el.getAttribute("contenteditable") == "false")
+                el.setAttribute("contenteditable", "true");
+        else
+                el.setAttribute("contenteditable", "false");
+}
+function isCompleted(element) {
+        let el = element;
+        if (el.classList.contains("completed"))
+                el.classList.remove("completed");
+        else
+                el.classList.add("completed");
+}
 function addTaskEnter(event) {
         event.preventDefault();
         addTask();
 }
 const form = document.getElementById('addTask');
 form.addEventListener('submit', addTaskEnter);
+///fac un for, verific daca elementul are clasa completed
+///daca o are il creez lista completed si o afisez,daca nu creez lista active si o afisez
